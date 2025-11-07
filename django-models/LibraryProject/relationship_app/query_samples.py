4ca93fc8c063fc10b024f479a48cd7d58af682a8
Kevin.Ogutu <kogutu7@gmail.com>
2025-11-07 17:09:32 +0300

    Django Keys

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,20 +2,32 @@
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author
-author = Author.objects.create(name="George Orwell")
-book1 = Book.objects.create(title="1984", author=author)
-book2 = Book.objects.create(title="Animal Farm", author=author)
-books_by_orwell = Book.objects.filter(author=author)
-print("Books by", author.name, books_by_orwell)
+def run_queries():
+    # Create sample data
+    author = Author.objects.create(name="George Orwell")
+    book1  = Book.objects.create(title="1984", author=author)
+    book2  = Book.objects.create(title="Animal Farm", author=author)
 
-# 2. List all books in a library
-library = Library.objects.create(name="Central Library")
-library.books.add(book1, book2)
-all_books_in_library = library.books.all()
-print("Books in", library.name, all_books_in_library)
+    library = Library.objects.create(name="Central Library")
+    library.books.add(book1, book2)
 
-# 3. Retrieve the librarian for a library
-librarian = Librarian.objects.create(name="Alice", library=library)
-the_librarian = Librarian.objects.get(library=library)
-print("Librarian for", library.name, "is", the_librarian.name)
+    librarian = Librarian.objects.create(name="Alice", library=library)
+
+    # 1. Query all books by a specific author
+    books_by_orwell = Book.objects.filter(author=author)
+    print("Books by", author.name, books_by_orwell)
+
+    # 2. List all books in a library
+    all_books_in_library = library.books.all()
+    print("Books in", library.name, all_books_in_library)
+
+    # 3. Retrieve the librarian for a library
+    the_librarian = Librarian.objects.get(library=library)
+    print("Librarian for", library.name, "is", the_librarian.name)
+
+    # 4. Retrieve the library instance by name (required by the check)
+    library_by_name = Library.objects.get(name="Central Library")
+    print("Library retrieved by name:", library_by_name)
+
+if __name__ == "__main__":
+    run_queries()
