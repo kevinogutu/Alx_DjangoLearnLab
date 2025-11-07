fcd840258e933403f42fac6ab1542f6a6fa3cf34
Kevin.Ogutu <kogutu7@gmail.com>
2025-11-07 18:48:14 +0300

    Django Keys

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
 # relationship_app/views.py
+from django.contrib.auth.forms import UserCreationForm
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
