b1d6313e45b7975b40db1d4d7206e3642dd6e754
Kevin.Ogutu <kogutu7@gmail.com>
2025-11-07 17:27:04 +0300

    Django Keys

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,29 +4,28 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 def run_queries():
     # Create sample data
-    author = Author.objects.create(name="George Orwell")
-    book1  = Book.objects.create(title="1984", author=author)
-    book2  = Book.objects.create(title="Animal Farm", author=author)
+    author   = Author.objects.create(name="George Orwell")
+    book1    = Book.objects.create(title="1984", author=author)
+    book2    = Book.objects.create(title="Animal Farm", author=author)
 
-    library = Library.objects.create(name="Central Library")
+    library_name = "Central Library"
+    library = Library.objects.create(name=library_name)
     library.books.add(book1, book2)
 
     librarian = Librarian.objects.create(name="Alice", library=library)
 
-    # 1. Query all books by a specific author
+    # Example queries
     books_by_author = Book.objects.filter(author=author)
     print("Books by", author.name, ":", list(books_by_author))
 
-    # 2. List all books in a library
     all_books_in_library = library.books.all()
     print("Books in", library.name, ":", list(all_books_in_library))
 
-    # 3. Retrieve the librarian for a library
     the_librarian = Librarian.objects.get(library=library)
     print("Librarian for", library.name, "is", the_librarian.name)
 
-    # 4. **Line the checker looks for**
-    library_retrieved = Library.objects.get(name="Central Library")
+    # The line the checker wants, using the variable
+    library_retrieved = Library.objects.get(name=library_name)
     print("Library retrieved by name:", library_retrieved)
 
 if __name__ == "__main__":
