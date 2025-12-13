61084d67422fa50899fd475dc15debfcad4d1704
Kevin.Ogutu <kogutu7@gmail.com>
2025-12-13 18:24:43 +0300

    Social Media Api

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        Token.objects.create(user=user)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return {
+            'token': token.key
+        }
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(
+        source='followers.count',
+        read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'bio',
+            'profile_picture',
+            'followers_count'
+        ]
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,30 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    UserProfileSerializer
+)
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
