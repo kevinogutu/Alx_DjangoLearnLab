ff6929d3e4d0dda17eb1bf8add2e2cb30050b6cb
Kevin.Ogutu <kogutu7@gmail.com>
2025-12-14 08:12:00 +0300

    User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
-
+CustomUser = get_user_model()
 User = get_user_model()
 from .models import User
 from .serializers import (
