27f7b10f4c52e41ec77b69b7472b5ec90fcd1d2e
Kevin.Ogutu <kogutu7@gmail.com>
2025-12-14 07:21:51 +0300

    Implementing Posts and Comments Functionality

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,31 @@
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+class PostPagination(PageNumberPagination):
+    page_size = 10
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    pagination_class = PostPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
